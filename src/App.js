47a8bf8879f4caaaa5bc0a2d1bbb17367b8d1573
christophersiem <christopher.siem@gmail.com>
2021-09-29 16:12:30 +0200

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
 
-    const welcomeMessage = "Hello course"
-
     return (
         <div>
-            <h1>{welcomeMessage}</h1>
+            <Header title="Rick & Morty App"/>
         </div>
     );
 }
