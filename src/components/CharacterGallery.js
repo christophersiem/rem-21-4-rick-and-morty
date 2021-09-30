b4f2ce1873c01fa7365a4fee78b5ac79dfef3493
Matthias Hiemer <matthias.hiemer@neuefische.de>
2021-09-30 11:38:47 +0200

    Add character gallery

---
+++ src/components/CharacterGallery.js
@@ -0,0 +1,13 @@
+import CharacterCard from "./CharacterCard";
+import './CharacterGallery.css'
+
+export default function CharacterGallery({characters}) {
+
+    return (
+        <section className="character-gallery">
+            {
+                characters.map(character => <CharacterCard key={character.id} character={character}/>)
+            }
+        </section>
+    )
+}
