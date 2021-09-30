38ff0aba05cc3e0fd61615b661c2d4f8dfa0a786
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-09-30 13:19:31 +0200

    Add CharacterState + Load Button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Header from "./components/Header";
 import characterResponse from "./characters.json"
 import CharacterGallery from "./components/CharacterGallery";
+import {useState} from "react";
 
 function App() {
 
-    const characters = characterResponse.results
+
+    const [characters, setCharacters] = useState([])
+
+    const handleButtonClick = () => {
+        setCharacters(characterResponse.results)
+    }
 
     return (
         <div>
             <Header title="Rick & Morty App"/>
+            <button onClick={handleButtonClick} >Load Characters</button>
             <CharacterGallery characters={characters} />
 
         </div>
