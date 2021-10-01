aefa437f2e45a9733996f58d230b9773b3e43795
Matthias Hiemer <matthias.hiemer@neuefische.de>
2021-10-01 13:50:09 +0200

    Add error handling in fetch method

--- src/services/rick-and-morty-api-service.js
+++ src/services/rick-and-morty-api-service.js
@@ -1,6 +1,12 @@
 
 export const fetchCharacters = (page) => {
     return fetch("https://rickandmortyapi.com/api/character/?page=" + page)
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error("Error while loading characters from Rick and Morty API!")
+            }
+        })
         .then(page => page.results)
 }
